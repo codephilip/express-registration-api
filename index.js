8b4a3fcb84359d7f7e58ffb43b04d3188d7d67fb
codephilip <philip@meiers.in>
2024-02-06 21:14:27 -0800

    global errorhandler

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(middleware1);
 app.use(middleware2);
-
+app.use(errorHandler);
 function errorHandler(err, req, res, next) {
   if (err) {
     res.send('<h1>Error here</h1>');
