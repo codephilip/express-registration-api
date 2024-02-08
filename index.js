69ebb9ead907b2dcfb8d1a7d5a3692c00860bd16
codephilip <philip@meiers.in>
2024-02-07 23:59:08 -0800

    index.js

--- index.js
+++ index.js
@@ -1,71 +1,71 @@
-const dotenv = require('dotenv');
-dotenv.config();
-
 const express = require('express');
 const mongoose = require('mongoose');
 const session = require('express-session');
-const MongoStore = require('connect-mongo');
-const bodyParser = require('body-parser');
-const { middleware1 } = require('./middleware/middleware1');
+var passport = require('passport');
+var crypto = require('crypto');
+var routes = require('./routes/routes');
+const connection = require('./config/db');
 
-const passport = require('passport');
-const crypto = require('crypto');
-// Assuming middleware2 is defined elsewhere or uncomment its definition below
+// Package documentation - https://www.npmjs.com/package/connect-mongo
+const MongoStore = require('connect-mongo')(session);
 
-const app = express();
-const dbString = process.env.MONGO_URI;
-const dbOptions = {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-};
+/**
+ * -------------- GENERAL SETUP ----------------
+ */
 
-mongoose.connect(dbString, dbOptions); // Connect using mongoose directly
+// Gives us access to variables set in the .env file via `process.env.VARIABLE_NAME` syntax
+require('dotenv').config();
 
-// Middleware
-app.use(express.json()); // Corrected
-app.use(express.urlencoded({ extended: true })); // Corrected
-app.use(bodyParser.json());
-app.use(middleware1);
-// app.use(middleware2); // Uncomment if middleware2 is defined
+// Create the Express application
+var app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+/**
+ * -------------- SESSION SETUP ----------------
+ */
+
+const sessionStore = new MongoStore({ mongooseConnection: connection, collection: 'sessions' });
 
 app.use(session({
-  secret: 'some secret', // This should be a random secret for production
+  secret: process.env.SECRET,
   resave: false,
   saveUninitialized: true,
-  store: MongoStore.create({ mongoUrl: dbString, collectionName: 'users' }), // Corrected to use MongoStore.create
+  store: sessionStore,
   cookie: {
-    maxAge: 1000 * 60 * 60 * 24 // Example: 24 hours
+    maxAge: 1000 * 60 * 60 * 24 // Equals 1 day (1 day * 24 hr/1 day * 60 min/1 hr * 60 sec/1 min * 1000 ms / 1 sec)
   }
 }));
 
-app.get('/', (req, res, next) => {
-  console.log('hi')
+/**
+ * -------------- PASSPORT AUTHENTICATION ----------------
+ */
+
+// Need to require the entire Passport config module so app.js knows about it
+require('./config/passport');
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use((req, res, next) => {
   console.log(req.session);
+  console.log(req.user);
+  next();
+});
 
-  if (req.session.viewCount) {
-    req.session.viewCount++;
-  } else {
-    req.session.viewCount = 1
-  }
+/**
+ * -------------- ROUTES ----------------
+ */
 
-  res.send(
-    `<h1>hello</h1>
-      <h2>You have visted ${req.session.viewCount} times</h2>`
-  )
+// Imports all of the routes from ./routes/index.js
+app.use(routes);
 
-})
 
-// Routes and error handler...
+/**
+ * -------------- SERVER ----------------
+ */
 
-// Example middleware2, uncomment or ensure it's defined elsewhere
-// function middleware2(req, res, next) {
-//   console.log('middleware2');
-//   req.customProp = 600;
-//   console.log(`custom prop: ${req.customProp}`);
-//   const newErr = new Error('its a Error.');
-//   next(newErr);
-// }
-
-// Start server
-app.listen(3001, () => console.log('Server running on port 3001'));
-
+// Server listens on http://localhost:3000
+app.listen(3000);
