543992782a75a93cea6d58f978630a18aa166f57
codephilip <philip@meiers.in>
2024-02-08 00:10:17 -0800

    console.log for when connection open

--- config/db.js
+++ config/db.js
@@ -5,9 +5,14 @@ require('dotenv').config();
 const conn = process.env.MONGO_URI;
 
 const connection = mongoose.createConnection(conn, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
+    // Options for the connection
 });
+
+// Log a success message if the connection is successful
+connection.once('open', () => {
+    console.log('Database connection successful');
+});
+
 
 // Expose the connection
 module.exports = connection;
