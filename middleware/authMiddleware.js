888cef90b9eb7cebba30d10348edfd53c85133a5
codephilip <philip@meiers.in>
2024-02-07 23:58:51 -0800

    fix authMiddleware missing bracket

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,17 +1,18 @@
 module.exports.isAuth = (req, res, next) => {
     if (req.isAuthenticated()) {
         next();
+    }
     else {
-            res.status(401).send('You are not authorized to view this page');
-            res.redirect('/login');
-        }
+        res.status(401).send('You are not authorized to view this page');
+        res.redirect('/login');
     }
+}
 
-    module.exports.isAdmin = (req, res, next) => {
-        if (req.isAuthenticated() && req.user.admin) {
-            next();
-        }
-        else {
-            res.status(401).send('You are not authorized to view this page');
-        }
+module.exports.isAdmin = (req, res, next) => {
+    if (req.isAuthenticated() && req.user.admin) {
+        next();
     }
+    else {
+        res.status(401).send('You are not authorized to view this page');
+    }
+}
